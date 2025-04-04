6043f77d06da932fb62014a11f8d82507b671d6f
Yoann Mahiout <mahiout@et.esiea.fr>
2025-04-04 15:28:23 +0200

    added test for get_glasses function

--- tests/test_copy_x.py
+++ tests/test_copy_x.py
@@ -7,6 +7,7 @@ import os
 # Add the path to jcc.py
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../jcc')))
 from jcc import set_clipboard
+from jcc import get_classes
 
 
 class TestCopyX(unittest.TestCase):
@@ -31,6 +32,24 @@ class TestCopyX(unittest.TestCase):
             set_clipboard("\"copied\"\n text\n"),
             subprocess.run(arg, shell=True, capture_output=True, text=True, check=True).stdout
         )
+
+    def test_get_classes(self):
+        folder = "data/jexample_proj/"
+        java_folder = folder + "java/"
+        reference_folder = folder + "ref/"
+
+        excl = []
+        main = False
+
+        res_classes = get_classes(java_folder, excl, main)
+        ref_classes = ""
+
+        with open(reference_folder + "reference", "r", encoding="utf-8") as f:
+            for line in f.readlines():
+                ref_classes += line
+        f.close()
+
+        self.assertEqual(res_classes, ref_classes)
 
 if __name__ == "__main__":
     unittest.main()
