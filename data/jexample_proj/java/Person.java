dc10a4cb7c8351c4bf28562bc54e6621b09fa424
Yoann Mahiout <mahiout@et.esiea.fr>
2025-04-04 17:59:22 +0200

    added data folder for testing

---
+++ data/jexample_proj/java/Person.java
@@ -0,0 +1,29 @@
+package jexample_proj.java;
+
+import java.util.*;
+
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
